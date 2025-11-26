ade007f7144beecc818524f8d21b910a59f6f5f2
liucaiwen <liucaiwenseven@163.com>
2025-11-26 15:20:00 +0800

    import:项目初始化

--- src/main/java/com/mini/server/RedisMiniServer.java
+++ src/main/java/com/mini/server/RedisMiniServer.java
@@ -1,5 +1,17 @@
 package com.mini.server;
 
+import com.mini.server.handler.StringHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
 /**
  * @author liucaiwen
  * 2025/11/26
@@ -8,20 +20,56 @@ public class RedisMiniServer implements RedisServer {
 
     private String host;
     private int port;
+    // bossGroup 用于接收连接
+    private EventLoopGroup bossGroup;
+    // workGroup 用于处理任务
+    private EventLoopGroup workerGroup;
+
+    private Channel serverChannel;
 
     public RedisMiniServer(String host, int port) {
         this.host = host;
         this.port = port;
+        this.bossGroup = new NioEventLoopGroup();
+        this.workerGroup = new NioEventLoopGroup();
     }
 
     @Override
     public void start() {
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new StringHandler());
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+        // 绑定host 和 端口
+        try {
+            serverChannel = bootstrap.bind(host, port).sync().channel();
+            System.out.println("RedisMiniServer started on port " + port);
+        } catch (InterruptedException e) {
+            stop();
+            Thread.currentThread().interrupt();
+        }
+
 
     }
 
     @Override
     public void stop() {
-
+        if (bossGroup != null) {
+            bossGroup.shutdownGracefully();
+        }
+        if (workerGroup != null) {
+            workerGroup.shutdownGracefully();
+        }
+        if (serverChannel != null) {
+            serverChannel.close();
+        }
     }
 
     public String getHost() {
@@ -38,5 +86,21 @@ public class RedisMiniServer implements RedisServer {
 
     public void setPort(int port) {
         this.port = port;
+    }
+
+    public EventLoopGroup getBossGroup() {
+        return bossGroup;
+    }
+
+    public void setBossGroup(EventLoopGroup bossGroup) {
+        this.bossGroup = bossGroup;
+    }
+
+    public EventLoopGroup getWorkerGroup() {
+        return workerGroup;
+    }
+
+    public void setWorkerGroup(EventLoopGroup workerGroup) {
+        this.workerGroup = workerGroup;
     }
 }
---
+++ src/main/java/com/mini/server/handler/StringHandler.java
@@ -0,0 +1,19 @@
+package com.mini.server.handler;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+/**
+ * @author liucaiwen
+ * 2025/11/26
+ */
+public class StringHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("收到消息" + msg);
+        ctx.channel().writeAndFlush("OK");
+    }
+}
